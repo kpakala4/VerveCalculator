81bfad65563b6ef983c1fbee9206b0657f599c44
kpakala4 <kpakala18@gmail.com>
2025-09-17 17:22:22 -0400

    Add Android calculator app with HyBid ads

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://android-sdk.im.pubnative.net/") }
+    }
+}
+
+rootProject.name = "VerveCalculator"
+include(":app")
